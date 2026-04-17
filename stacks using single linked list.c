b87d8ea9f6b8dc0ad5c416e6b1823d218024b0dd
Ferozsrec <peerlaferozsrec@gmail.com>
2026-04-17 20:05:04 +0530

    Create stacks using single linked list.c

---
+++ stacks using single linked list.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *top = NULL, *temp, *new_node;
+
+void push();
+void pop();
+void display();
+
+void main()
+{
+    int choice;
+
+    while (1)
+    {
+        printf("\n*******");
+        printf("\n1. PUSH");
+        printf("\n2. POP");
+        printf("\n3. DISPLAY");
+        printf("\n4. EXIT");
+        printf("\n*******");
+
+        printf("\nEnter Your Choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            push();
+            break;
+
+        case 2:
+            pop();
+            break;
+
+        case 3:
+            display();
+            break;
+
+        case 4:
+            exit(0);
+        }
+    }
+}
+
+// PUSH
+void push()
+{
+    new_node = (struct node *)malloc(sizeof(struct node));
+
+    printf("Enter data: ");
+    scanf("%d", &new_node->data);
+
+    new_node->next = top;
+    top = new_node;
+}
+
+// POP
+void pop()
+{
+    if (top == NULL)
+    {
+        printf("Stack Underflow");
+    }
+    else
+    {
+        temp = top;
+        printf("Deleted element is %d", top->data);
+        top = top->next;
+        free(temp);
+    }
+}
+
+// DISPLAY
+void display()
+{
+    if (top == NULL)
+    {
+        printf("Stack is empty");
+    }
+    else
+    {
+        temp = top;
+        while (temp != NULL)
+        {
+            printf("%d -> ", temp->data);
+            temp = temp->next;
+        }
+    }
+}
